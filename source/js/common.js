8fde0ecaa96d417a58cbb8ca72b50c8a91f795e3
Murad <abishlimurad@gmail.com>
2023-08-14 00:49:55 +0300

    100%

--- source/js/common.js
+++ source/js/common.js
@@ -1,7 +1,16 @@
 if (Swiper) {
   const quotersSlider = new Swiper('.quotes__hidden', {
-    slidesPerView: 1, // Отображать три слайда
-    centeredSlides: true,
+		effect: 'coverflow',
+		slidesPerView: 'auto',
+    roundLengths: true,
+		centeredSlides: true,
+    	coverflowEffect: {
+			rotate: 0,
+			stretch: 80,
+			depth: 200,
+			modifier: 1, // 2,3
+			slideShadows : false,
+		},// Отображать три слайда
     spaceBetween: 20,
     pagination: {
       el: '.quotes__slider-pag', // Элемент контейнера буллетов
